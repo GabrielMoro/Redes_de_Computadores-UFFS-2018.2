ad10b62142a442778f60bfc34c58b3a5aa73427b
Eduardo Tonatto <edtonatto@gmail.com>
2018-09-29 20:26:44 -0300

    Header

--- T1-Roteador_UDP_Dijkstra/router.h
+++ T1-Roteador_UDP_Dijkstra/router.h
@@ -25,10 +25,16 @@ typedef struct{                     // Estrutura das tabelas de roteamentos
   int path[N_ROT];
 }Table;
 
+void die(char *s);
+void *receive(int id);
+void create_message(int id);
+void send(int this_id, int next_id);
 void create_router(int r_ID);
 void create_links(int tab[N_ROT][N_ROT]);
-int verify(int *v, int value);
+int  verify(int *v, int value);
 void removev(int *v, int value);
-int findsminor(int *v, int *open);
+int  findsminor(int *v, int *open);
 void dijkstra(int tab_rot[N_ROT][N_ROT], int start);
 void backtracking(int start, int prev[N_ROT]);
+void pathcost(int start, int tab_rot[N_ROT][N_ROT]);
+int  toint(char *str);
