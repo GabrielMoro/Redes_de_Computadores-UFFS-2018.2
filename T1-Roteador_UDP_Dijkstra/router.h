db2560993c5f0bfa163284755fde306ff9919d88
Eduardo Tonatto <edtonatto@gmail.com>
2018-09-29 18:45:19 -0300

    Alterado estrutura de mensagens e roteadores

--- T1-Roteador_UDP_Dijkstra/router.h
+++ T1-Roteador_UDP_Dijkstra/router.h
@@ -5,19 +5,19 @@
 #include <sys/socket.h>
 #include <pthread.h>
 
-#define QUEUE_SIZE 93     // Tamanho das filas dos roteadores
+#define QUEUE_SIZE 100     // Tamanho das filas dos roteadores
 #define MESSAGE_SIZE 100  // Mensagens são limitadas a 100 caracteres
 #define N_ROT 6           // Número de roteadores
 
 typedef struct{           // Estrutura dos roteadores
   int id, port;
-  int message_in, message_out;
+  int message_in[QUEUE_SIZE], message_out[QUEUE_SIZE];
   char ip[30];
 }Router;
 
 typedef struct{                     // Estrutura dos pacotes
   int source, destination, type;    // Header
-  char content[100];                // Mensagem
+  char content[MESSAGE_SIZE];                // Mensagem
 }Package;
 
 typedef struct{                     // Estrutura das tabelas de roteamentos
